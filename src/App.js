0796aeec247d2730e85a994e3196d5abb3cb656a
Aniket Waliyan <aniket.wali007@gmail.com>
2021-04-10 03:47:13 +0530

    Added Utils which contains songs info. Added PLay functionality in PLayer.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import React, {useState} from 'react';
+// Import Styles
 import './styles/app.scss';
+// Import Components
 import Songs from './components/Songs';
 import Player from './components/Player';
+// Import Util
+import data from './util';
 
 function App() {
+  // State
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="App">
-      <Songs />
-      <Player />
+      <Songs currentSong={currentSong}/>
+      <Player currentSong={currentSong} isPlaying={isPlaying} setIsPlaying={setIsPlaying} />
     </div>
   );
 }
